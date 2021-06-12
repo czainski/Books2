493bedbb0e3c68d9559efb1ceee1f12f0c923296
czainski <38532082+czainski@users.noreply.github.com>
2021-06-12 15:08:44 +0200

    V1

---
+++ Books/T-SQL/Pivot.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Grades(
+  [Student] VARCHAR(50),
+  [Subject] VARCHAR(50),
+  [Marks]   INT
+)
+GO
+ 
+INSERT INTO Grades VALUES 
+('Jacob','Mathematics',100),
+('Jacob','Science',95),
+('Jacob','Geography',90),
+('Amilee','Mathematics',90),
+('Amilee','Science',90),
+('Amilee','Geography',100)
+GO
---
+++ Books/T-SQL/Pivot2.sql
@@ -0,0 +1,22 @@
+﻿  SELECT
+    [Student],
+    [Subject],
+    [Marks]
+  FROM Grades  where Student like 'A%'
+;
+SELECT * FROM (
+  SELECT
+    [Student],
+    [Subject],
+    [Marks]
+  FROM Grades where Student like 'A%'
+) StudentResults
+PIVOT (
+  SUM([Marks])
+  FOR [Subject]
+  IN (
+    [Mathematics],
+    [Science],
+    [Geography]
+  )
+) AS PivotTable
