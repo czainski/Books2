d266e6ee1f8b054aabaa9954d5a43ea9b1e672d8
czainski <38532082+czainski@users.noreply.github.com>
2021-06-14 13:43:11 +0200

    V1

--- Books/WebAPI/Doglio F. - REST API Development with Node.js, 2nd Edition - 2018/rest-api-dev-node.js-master/lib/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	helpers: require("./helpers"),
-	logger: require("./logger"),
-	controllers: require("../controllers"),
-	schemas: require("../schemas"),
-	schemaValidator: require("./schemaValidator"),
-	db: require("./db")
-}
